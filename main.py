965129d22f5562248a3c7a335f8375a2945d3be9
vitorhsm14@gmail.com <ap99949188>
2021-04-26 15:49:15 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+# Variable Declaration
+name = "Vitor"
+age = 27
+
+# Case Sensitive
+Name = "Vitor"  # <> name
+
+# Variable type
+typeStr = type(name)
+
+# Input
+get_name = input('What is your name? ')
+
+# String length
+nameLength = len(get_name)
+
+# Number concatenation
+numberConcat = str(5) + "5"
+
+# String detection
+x = "abc".isnumeric()  # false
+y = "123".isnumeric()  # true
+
+# Print
+print(nameLength)
